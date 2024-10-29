fc79b4cbd12867e9cbf590623474121d63ed4b4e
eNKay <nguyenkhai24082408@gmail.com>
2024-10-29 15:42:37 +0700

    feat: Add repository interfaces for data access
    - Add IBrandRepository.cs to define CRUD operations for Brand entities.
    - Add ICategoryRepository.cs to define CRUD operations for Category entities.
    - Add IEmployeeRepository.cs to define operations for Employee entities, including saving and retrieving by email.
    - Add IProductRepository.cs to define CRUD operations for Product entities.

---
+++ POSSystem/Repository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using POSSystem.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace POSSystem.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetAllCategories();
+        Task<Category> GetCategoryById(int id);
+        Task AddCategory(Category category);
+        Task UpdateCategory(Category category);
+        Task DeleteCategory(int id);
+    }
+}
