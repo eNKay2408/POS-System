3f1b70b5b87dc4ee5f84f75aea821d79e17a79e8
eNKay <nguyenkhai24082408@gmail.com>
2024-10-29 15:39:24 +0700

    feat: Add value converters for data binding
    - Add DecimalToStringConverter.cs to convert decimal values to strings and vice versa.
    - Add IntToStringConverter.cs to convert integer values to strings and vice versa.
    - Implement IValueConverter interface for both converters.

---
+++ POSSystem/Converters/DecimalToStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+
+namespace POSSystem.Converters
+{
+    public class DecimalToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value?.ToString() ?? string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (decimal.TryParse(value as string, out var result))
+            {
+                return result;
+            }
+            return 0m; 
+        }
+    }
+}
---
+++ POSSystem/Converters/IntToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.UI.Xaml.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POSSystem.Converters
+{
+    public class IntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value?.ToString() ?? string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            if (int.TryParse(value as string, out var result))
+            {
+                return result;
+            }
+            return null;
+        }
+    }
+}
