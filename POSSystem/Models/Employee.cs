4967dce89c982d9af15b8e21becff754f2fade4b
eNKay <nguyenkhai24082408@gmail.com>
2024-10-29 15:41:05 +0700

    feat: Add model classes for POS system
    - Add Brand.cs to represent brand entities with Id, Name, and Index properties.
    - Add Category.cs to represent category entities with Id, Name, and Index properties.
    - Add Employee.cs to represent employee entities with Id, Name, Email, Password, and Index properties.
    - Add Product.cs to represent product entities with Id, Name, Price, Stock, CategoryId, CategoryName, BrandId, BrandName, and Index properties.

---
+++ POSSystem/Models/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace POSSystem.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string? Password { get; set; }
+        public int Index { get; set; }
+    }
+}
