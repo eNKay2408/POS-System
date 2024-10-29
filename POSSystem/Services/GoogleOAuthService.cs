998bc6f32bc5fc6bd150289caa9512087a419e91
eNKay <nguyenkhai24082408@gmail.com>
2024-10-29 15:44:56 +0700

    feat: Add Google OAuth service
    - Add GoogleOAuthService.cs to handle Google OAuth authentication.
    - Implement method to authenticate and retrieve employee information using Google APIs.
    - Use ConfigHelper to load Google client credentials from appsettings.json.

---
+++ POSSystem/Services/GoogleOAuthService.cs
@@ -0,0 +1,55 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Services;
+using Google.Apis.Oauth2.v2;
+using Google.Apis.Util.Store;
+using System.Threading;
+using System.Threading.Tasks;
+using POSSystem.Models;
+using POSSystem.Helpers;
+
+namespace POSSystem.Services
+{
+    public class GoogleOAuthService
+    {
+        private static readonly string[] _scopes = { Oauth2Service.Scope.UserinfoEmail, Oauth2Service.Scope.UserinfoProfile };
+        private string _applicationName = "POS System";
+
+        private ConfigHelper _configHelper;
+
+        public GoogleOAuthService()
+        {
+            _configHelper = new ConfigHelper();
+        }
+
+        public async Task<Employee> AuthenticateAsync()
+        {
+            UserCredential credential;
+
+            var clientId = _configHelper.GetGoogleClientId();
+            var clientSecret = _configHelper.GetGoogleClientSecret();
+
+            var clientSecrets = new ClientSecrets
+            {
+                ClientId = clientId,
+                ClientSecret = clientSecret
+            };
+
+            credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                clientSecrets,
+                _scopes,
+                "employee",
+                CancellationToken.None,
+                new NullDataStore());
+
+            var oauthService = new Oauth2Service(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = _applicationName,
+            });
+
+            var employeeInfo = await oauthService.Userinfo.Get().ExecuteAsync();
+
+            return new Employee { Name = employeeInfo.Name, Email = employeeInfo.Email };
+        }
+    }
+}
