316d612c7e28b84216250d48c49ccf04fa642ada
Ma Cat Huynh <uynhhuc810@gmail.com>
2024-11-06 23:48:05 +0700

    feat: add db migrations

---
+++ database_setup/migrations/20241106164257_create_brand.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.raw(`
+    CREATE TABLE brand (
+      id SERIAL PRIMARY KEY,
+      name VARCHAR(255) NOT NULL)`);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.raw('DROP TABLE brand');
+};
---
+++ database_setup/migrations/20241106164430_create_product.js
@@ -0,0 +1,24 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = async function(knex) {
+  await knex.raw(` 
+    CREATE TABLE product (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price NUMERIC(18, 2) NOT NULL,
+    stock INTEGER NOT NULL,
+    categoryid INTEGER NOT NULL,
+    brandid INTEGER,
+    FOREIGN KEY (categoryid) REFERENCES category(id),
+    FOREIGN KEY (brandid) REFERENCES brand(id))`);
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = async function(knex) {
+  await knex.raw('DROP TABLE product');
+};
