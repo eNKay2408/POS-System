767157d25d104c622841298f6a49dbfbe63c1eae
eNKay <nguyenkhai24082408@gmail.com>
2024-11-18 22:03:09 +0700

    test: Add unit tests for IntToStringConverter
    - Added unit tests for Convert and ConvertBack methods in IntToStringConverter

---
+++ POSSystem.Tests/Converters/IntToStringConverterTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POSSystem.Converters;
+
+namespace POSSystem.Tests.Converters
+{
+    [TestClass()]
+    public class IntToStringConverterTests
+    {
+        private IntToStringConverter _converter;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _converter = new IntToStringConverter();
+        }
+
+        [TestMethod()]
+        [DataRow(123, "123")]
+        [DataRow(0, "0")]
+        [DataRow(-123, "-123")]
+        [DataRow(null, "")]
+        public void Convert_ShouldReturnExpectedString(object input, object expected)
+        {
+            // Act
+            var result = _converter.Convert(input, typeof(string), null, "");
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        [DataRow("123", 123)]
+        [DataRow("0", 0)]
+        [DataRow("-123", -123)]
+        [DataRow("invalid", null)]
+        [DataRow("", null)]
+        [DataRow(null, null)]
+        public void ConvertBack_ShouldReturnExpectedInt(object input, object expected)
+        {
+            // Act
+            var result = _converter.ConvertBack(input, typeof(int), null, "");
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
