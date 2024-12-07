91c564121332e41c934176db7c0aa359db90a1bf
eNKay <nguyenkhai24082408@gmail.com>
2024-12-07 21:33:47 +0700

    refactor: Add IConfigHelper interface for ConfigHelper
    - Created IConfigHelper interface for ConfigHelper to improve testability
    - Updated ConfigHelper to implement IConfigHelper

---
+++ POSSystem/Helpers/IConfigHelper.cs
@@ -0,0 +1,13 @@
+﻿namespace POSSystem.Helpers
+{
+    public interface IConfigHelper
+    {
+        string GetStripeSecretKey();
+        string GetGoogleClientId();
+        string GetGoogleClientSecret();
+        string GetDatabaseHost();
+        string GetDatabaseUsername();
+        string GetDatabasePassword();
+        string GetDatabaseName();
+    }
+}
