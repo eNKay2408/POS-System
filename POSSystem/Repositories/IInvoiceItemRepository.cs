bdeeb0446b449b8e69812be592c8df30717f128a
eNKay <nguyenkhai24082408@gmail.com>
2024-12-24 12:06:21 +0700

    feat: Add InvoiceItemRepository for managing invoice items
    - Created IInvoiceItemRepository interface with methods for CRUD operations on invoice items
    - Implemented InvoiceItemRepository class with methods to add, update, delete, and retrieve invoice items

---
+++ POSSystem/Repositories/IInvoiceItemRepository.cs
@@ -0,0 +1,15 @@
+﻿using POSSystem.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace POSSystem.Repositories
+{
+    public interface IInvoiceItemRepository
+    {
+        public Task<List<InvoiceItem>> GetInvoiceItemsByInvoiceId(int invoiceId);
+        public Task AddInvoiceItem(InvoiceItem invoiceItem);
+        public Task UpdateInvoiceItem(InvoiceItem invoiceItem);
+        public Task DeleteInvoiceItem(int invoiceItemId);
+        public Task DeleteInvoiceItemsByInvoiceId(int invoiceId);
+    }
+}
