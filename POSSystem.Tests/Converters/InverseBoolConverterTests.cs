8afbd19ab9253553b0a621b0cb8ea3db63ddc690
eNKay <nguyenkhai24082408@gmail.com>
2024-12-31 21:16:54 +0700

    test: Add unit tests for InverseBoolConverter
    - Added tests for Convert, ConvertBack method with various boolean inputs

---
+++ POSSystem.Tests/Converters/InverseBoolConverterTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POSSystem.Converters;
+
+namespace POSSystem.Tests.Converters
+{
+    [TestClass()]
+    public class InverseBoolConverterTests
+    {
+        private InverseBoolConverter _converter;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _converter = new InverseBoolConverter();
+        }
+
+        [TestMethod()]
+        [DataRow(true, false)]
+        [DataRow(false, true)]
+        [DataRow(null, null)]
+        public void Convert_ShouldReturnExpectedBool(object input, object expected)
+        {
+            // Act
+            var result = _converter.Convert(input, typeof(bool), null, "");
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod()]
+        [DataRow(true, false)]
+        [DataRow(false, true)]
+        [DataRow(null, null)]
+        public void ConvertBack_ShouldReturnExpectedBool(object input, object expected)
+        {
+            // Act
+            var result = _converter.ConvertBack(input, typeof(bool), null, "");
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
